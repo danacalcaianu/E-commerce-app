3840d1cbbdde63a092e7021ef9a2995ac5306af6
Daniela Calcaianu <danacalcaianu@yahoo.com>
2018-05-24 09:31:02 +0300

    put default photo link in a variable

--- frontend/src/app/products-list/products-list.component.ts
+++ frontend/src/app/products-list/products-list.component.ts
@@ -8,9 +8,13 @@ import { products } from './cupcakes-data';
 })
 export class ProductsList implements OnInit {
   cupcakes: any;
+  defaultImage: any;
+
   constructor() {
     this.cupcakes = products;
-   }
+    this.defaultImage = 'https://cdn0.iconfinder.com/data/icons/sweets-glyph-black/2048/Cherry_cupcake-512.png';
+  }
+
   ngOnInit() {
   }
 
